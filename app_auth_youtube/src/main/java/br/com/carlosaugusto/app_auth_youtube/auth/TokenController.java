6589e281cd474c8431a4dba30a714b608e96a7ab
Carlos Augusto <carlos.brandao@etipi.pi.gov.br>
2024-04-18 21:08:48 -0300

    checkpoint

--- app_auth_youtube/src/main/java/br/com/carlosaugusto/app_auth_youtube/auth/TokenController.java
+++ app_auth_youtube/src/main/java/br/com/carlosaugusto/app_auth_youtube/auth/TokenController.java
@@ -1,16 +1,18 @@
 package br.com.carlosaugusto.app_auth_youtube.auth;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RequestMapping("/token")
 @RestController
+@RequestMapping("/token")
 public class TokenController {
 
   @PostMapping("/")
@@ -21,10 +23,16 @@ public class TokenController {
     headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
     MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+    formData.add("client_id", user.clienttId);
+    formData.add("username", user.username);
+    formData.add("password", user.password);
+    formData.add("grantType", user.grantType);
 
-    rt.postForEntity();
+    HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(formData, headers);
+
+    rt.postForEntity(entity, String.class);
   }
 
-  public record User(String password, String clienttId, String grantType, String user) {
+  public record User(String password, String clienttId, String grantType, String username) {
   }
 }
