9fddfc2d31522df1d3256a15e22c9d3bb4eef136
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-04-19 11:52:09 -0300

    feat: validating roles for each router

--- app_auth_youtube/src/main/java/br/com/carlosaugusto/app_auth_youtube/controller/ProductController.java
+++ app_auth_youtube/src/main/java/br/com/carlosaugusto/app_auth_youtube/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.carlosaugusto.app_auth_youtube.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +16,13 @@ public class ProductController {
   }
 
   @GetMapping("/")
+  @PreAuthorize("hasRole('USER')")
   public String list() {
     return "";
   }
 
   @PostMapping("/")
+  @PreAuthorize("hasRole('ADMIN')")
   public String create() {
     return "";
   }
